ad90982fb9e7c52f1b1024fe9507b90a57502992
Erik <erikgregorywebb@gmail.com>
2023-11-03 00:31:35 -0400

    Create tsa-passenger-volumes.r

---
+++ jobs/tsa-passenger-volumes.r
@@ -0,0 +1,27 @@
+# import packages
+library(tidyverse)
+library(dplyr)
+library(httr)
+library(jsonlite)
+library(aws.s3)
+
+# get current datetime
+datetime = Sys.time()
+
+# import
+url = 'https://www.tsa.gov/travel/passenger-volumes'
+page = read_html(url)
+table = page %>% html_table() %>% first()
+
+# clean
+df = table %>%
+  pivot_longer(., !Date,  names_to = "year", values_to = "passenger_volume") %>%
+  rename(date = Date) %>%
+  mutate(passenger_volume = as.numeric(gsub(",", "", passenger_volume))) %>%
+  mutate(scraped_at = datetime)
+
+####### EXPORT ####### 
+writeToS3 = function(file,bucket,filename){
+  s3write_using(file, FUN = write.csv, bucket = bucket, object = filename)
+}
+writeToS3(df, 'egw-data-dumps', paste('tsa/tsa-passenger-volumes-' , format(datetime, '%Y-%m-%d-%H-%M-%S'), '.csv', sep = ''))
